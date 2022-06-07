64db96abf8189631a1f63f6d0c94cfccde851940
Allan <allan.averlan@laposte.net>
2022-06-07 16:54:15 +0200

    Maquettage

---
+++ TP/Cours JS/Exo 1/js/main.js
@@ -0,0 +1,21 @@
+/* Alert en JS
+alert('Premier cour JS')
+*/
+
+/* Constante
+ nom="Allan";
+const age = 26;
+const moyenne= 18.5;
+const isCool=true;
+const x=null;
+const y=undefined;
+*/
+
+/* Constante + affichage
+ nom = "Allan";
+let age = 26;
+console.log(age);
+age = 26;
+console.log(age);
+console.log("Mon age est : " + age); // affichage soigné
+*/
