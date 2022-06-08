f6331f7b0d5ca2223f635753e7d4990653ce52ce
Allan <allan.averlan@laposte.net>
2022-06-08 15:30:55 +0200

    JS

--- TP/Cours JS/Exo 1/js/main.js
+++ TP/Cours JS/Exo 1/js/main.js
@@ -16,14 +16,14 @@ document.getElementById("age").innerHTML = "J'ai " + age + " ans";
 document.getElementById("add").innerHTML = "Mon adresse est : " + adresse + "," + code;
 */
 
-const nom = "Julien"; //string
-const age = 33; //number
-const moyenne = 18.5; //number
+const nom = "Allan"; //string
+const age = 26; //number
+const moyenne = 16.8; //number
 const isCool = true; // boolean
 const x = null; // object : type generique, en JS tout est objet
 const y = undefined; // var inexistante
 
-console.log("La variable nom est de type : " + typeof nom + ",la valeur est : " + nom); // ES5
+console.log("La variable nom est de type : " + typeof nom + ",la valeur est : " + nom + " qui est de " + nom.length + " caractères"); // ES5
 console.log("La variable age est de type : " + typeof age + ",la valeur est : " + age); // ES5
 console.log("La variable moyenne est de type : " + typeof moyenne + ",la valeur est : " + moyenne); // ES5
 console.log("La variable isCool est de type : " + typeof isCool + ",la valeur est : " + isCool); // ES5
@@ -32,9 +32,46 @@ console.log("La variable y est de type : " + typeof y + ",la valeur est : " + y)
 
 console.log("=========================");
 
-console.log(`La variable nom est de type ${typeof nom} et la valeur est ${nom}`); // ES6
-console.log(`La variableage est de type ${typeof age} et la valeur est ${age}`); // ES6
+console.log(`La variable nom est de type ${typeof nom} et la valeur est ${nom} qui est de ${nom.length} caractères`); // ES6
+console.log(`La variable age est de type ${typeof age} et la valeur est ${age}`); // ES6
 console.log(`La variable moyenne est de type ${typeof moyenne} et la valeur est ${moyenne}`); // ES6
 console.log(`La variable isCool  est de type ${typeof isCool} et la valeur est ${isCool}`); // ES6
 console.log(`La variable x est de type ${typeof x} et la valeur est ${x}`); // ES6
 console.log(`La variable y est de type ${typeof y} et la valeur est ${y}`); // ES6
+
+console.log("=========================");
+
+console.log(`Mon nom en maj : ${nom.toUpperCase()}`); // nom en maj
+console.log(`Mon nom en min : ${nom.toLowerCase()}`); // nom en min
+console.log(`Sous chaine : ${nom.substring(0,3)}`); // extrait une sous chaine, les index de 0 à 3 ==> 3 caract
+
+console.log("=========================");
+
+console.log(`Sous chaine : ${nom.substring(0,4).toUpperCase()}`); // prend les 4 caract et les transforme en maj
+console.log(nom.split('')); // decoupe la chaine en caractères
+const nomComplet = "Dalian Le Boss";
+console.log(nomComplet.split(' ')); // decoupe la chaine selon un séparateur (dans ce cas, c'est l'espace) 3 mots
+
+document.getElementById("age").innerHTML = `J'ai ${age} ans`;
+
+console.log("=========================");
+
+// decla de tableaux
+const numbers = new Array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+const fruits = ["mangue", "pastèque", "poire", "orange", "ananas", "pomme", "abricot", "peche"];
+
+// acces a un element du tableau
+console.log(fruits);
+console.log(fruits[0]); // acces au 1ere element
+console.log(fruits[1]); // acces au 2eme element
+console.log(fruits[2]); // acces au 3eme element
+console.log(fruits[3]); // acces au 4eme element
+fruits.unshift("fraise"); // ajout au debut du tableau
+console.log(fruits);
+fruits.push("banane"); // ajout en fin de tableau
+console.log(fruits);
+fruits.shift(); // supprimer le 1er element
+fruits.pop(); // supprimer le dernier element
+console.log(fruits.indexOf("orange")); // retourne l’index de l'element orange
+console.log(Array.isArray(fruits)); // true
+console.log(fruits);
