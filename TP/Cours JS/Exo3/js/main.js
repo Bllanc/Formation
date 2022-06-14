96152741c946985a6056550169a656c3852e25d0
Allan <allan.averlan@laposte.net>
2022-06-14 15:16:19 +0200

    ajout js

--- TP/Cours JS/Exo3/js/main.js
+++ TP/Cours JS/Exo3/js/main.js
@@ -21,6 +21,22 @@ let tab = moyennes.map(moyenne => {
 });
 */
 
+
+/*
+moyennes.map(moyenne => {
+    let msg = "";
+    if (moyenne >= 10) {
+        msg = "réussis";
+    } else {
+        msg = "échoué";
+    }
+    document.write(`Note : ${moyenne}, L'éléve à ${msg} <br>`)
+}); 
+*/
+
+
+
+
 let tab = moyennes.map((moyenne) => document.write(moyenne >= 10 ? `Note ${moyenne} : -  L'eleve à reussis. <br>` : `Note ${moyenne} : - L'eleve à échoué. <br>`)); // Affichage sur le body.
 
 console.log(moyennes.map((moyenne) => moyenne >= 10 ? `L'eleve à reussis. <br>` : `L'eleve à échoué. <br>`)); // Affichage dans la console.
