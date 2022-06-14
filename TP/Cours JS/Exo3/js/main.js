95b5d63360979ed58ee20ed665752d4f6b198b59
Allan <allan.averlan@laposte.net>
2022-06-14 14:55:40 +0200

    modif js

--- TP/Cours JS/Exo3/js/main.js
+++ TP/Cours JS/Exo3/js/main.js
@@ -1,5 +1,26 @@
 let moyennes = [10, 20, 11, 14, 19, 07, 02, 13, 17, 06];
 
-moyennes.map((moyenne) => document.write(moyenne >= 10 ? `L'eleve à reussis. <br>` : `L'eleve à échoué. <br>`)); // Affichage sur le body.
+/*
+moyennes.map();
+moyennes.map(cb);
+moyennes.map(
+    () => {
+
+    }
+);
+*/
+
+
+/*
+let tab = moyennes.map(moyenne => {
+    if (moyenne >= 10) {
+        document.write("note : " + moyenne + " " + " - L'eleve à reussis. <br>");
+    } else {
+        document.write("note : " + moyenne + " " + " - L'eleve à échoué. <br>");
+    }
+});
+*/
+
+let tab = moyennes.map((moyenne) => document.write(moyenne >= 10 ? `Note ${moyenne} : -  L'eleve à reussis. <br>` : `Note ${moyenne} : - L'eleve à échoué. <br>`)); // Affichage sur le body.
 
 console.log(moyennes.map((moyenne) => moyenne >= 10 ? `L'eleve à reussis. <br>` : `L'eleve à échoué. <br>`)); // Affichage dans la console.
