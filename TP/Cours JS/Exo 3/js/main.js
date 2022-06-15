7509419a346f33939ce5d23d20471cc9f06e7ee3
Allan <allan.averlan@laposte.net>
2022-06-15 09:22:53 +0200

    ajout JS

--- TP/Cours JS/Exo 3/js/main.js
+++ TP/Cours JS/Exo 3/js/main.js
@@ -13,10 +13,10 @@ moyennes.map(
 
 /*
 let tab = moyennes.map(moyenne => {
-    if (moyenne >= 10) {
-        document.write("note : " + moyenne + " " + " - L'eleve à reussis. <br>");
-    } else {
-        document.write("note : " + moyenne + " " + " - L'eleve à échoué. <br>");
+    if (moyenne >= 10) { // Si sup a 10 
+        document.write("note : " + moyenne + " " + " - L'eleve à reussis. <br>"); // On affiche sa 
+    } else { // sinon
+        document.write("note : " + moyenne + " " + " - L'eleve à échoué. <br>"); // On affiche sa 
     }
 });
 */
@@ -25,9 +25,9 @@ let tab = moyennes.map(moyenne => {
 /*
 moyennes.map(moyenne => {
     let msg = "";
-    if (moyenne >= 10) {
+    if (moyenne >= 10) { // Si sup a 10 
         msg = "réussis";
-    } else {
+    } else { // Sinon
         msg = "échoué";
     }
     document.write(`Note : ${moyenne}, L'éléve à ${msg} <br>`)
@@ -38,5 +38,6 @@ moyennes.map(moyenne => {
 
 
 let tab = moyennes.map((moyenne) => document.write(moyenne >= 10 ? `Note ${moyenne} : -  L'eleve à reussis. <br>` : `Note ${moyenne} : - L'eleve à échoué. <br>`)); // Affichage sur le body.
+
 
 console.log(moyennes.map((moyenne) => moyenne >= 10 ? `L'eleve à reussis. <br>` : `L'eleve à échoué. <br>`)); // Affichage dans la console.
---
+++ TP/Cours JS/Exo 4/js/main.js
@@ -0,0 +1,36 @@
+primes = [
+    { id: 1, perf: 10 },
+    { id: 2, perf: 56 },
+    { id: 3, perf: 41.25 },
+    { id: 4, perf: 45 },
+    { id: 5, perf: 87.45 },
+    { id: 6, perf: 96 },
+    { id: 7, perf: 91.13 },
+    { id: 8, perf: 82 },
+    { id: 9, perf: 52.47 }
+]
+
+
+
+//pour chacun des employés, calculer sa prime et faire l'affichage suivant:
+//id : 1, perf : 23.63, prime : 0
+//regle de calcul de prime : 10% si la performance depasse les 50%
+
+
+
+
+/*
+primes.map(prime => {
+    let msg = "";
+    if (prime.perf > 50) {
+        msg = `id : ${prime.id} perf : ${prime.perf} prime = 10% <br>`;
+    } else {
+        msg = `id : ${prime.id} perf : ${prime.perf} prime = 0% <br>`;
+    }
+    document.write(msg)
+});
+*/
+
+let tab = primes.map((prime) => document.write(prime.perf > 50 ? `id : ${prime.id} | perf : ${prime.perf} | prime = 10% <br>` : `id : ${prime.id} | perf : ${prime.perf} | prime = 0% <br>`)) // Afficahge dans le body 
+
+console.log(primes.map((prime) => prime.perf > 50 ? `id : ${prime.id} | perf : ${prime.perf} | prime = 10% <br>` : `id : ${prime.id} | perf : ${prime.perf} | prime = 0% <br>`)); // Affichage dans la console
