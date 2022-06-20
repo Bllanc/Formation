fc217dfe8c96d1cce8dc52ec699abb64e6e5d19a
Allan <allan.averlan@laposte.net>
2022-06-20 15:49:33 +0200

    correction

--- TP/Cours JS/Exo 9/js/main.js
+++ TP/Cours JS/Exo 9/js/main.js
@@ -7,17 +7,47 @@ function remplir() {
     for (let i = 0; i <= 100; i++) { // Pour i = 1; i inf à 99; on incrémente de 2
         tab1.push(i); // Push de i dans une variable tableau
     };
+    return tab1;
 };
 
 function paire() {
-    tab1.map((variable) => {
+    tab1.map((variable) => { // Parcour du tableau tab 1
         if (variable % 2 == 0) { // Si valeur % 2 == 0 alors pair
-            document.write(variable);
+            document.write(`${variable} | `);
         }
+        return variable;
     })
+
 };
 remplir();
 paire();
+
+document.write(`<br><br>Version de Rayen : <br><br> `);
+
+function nombres() {
+    let tab = [];
+    for (let i = 0; i <= 100; i++) {
+        tab.push(i);
+    }
+    return tab;
+}
+
+function pairs() {
+    let tabPairs = nombres();
+    let nbPairs = [];
+    for (let i = 0; i < tabPairs.length; i = i + 2) {
+        nbPairs.push(i);
+    }
+    return nbPairs;
+}
+
+document.write(pairs());
+
+
+
+
+
+
 
 /*
 Lecture de l'énoncé
