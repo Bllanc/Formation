3e866207ced1e3052fb0f1d5e60cca453492a26e
Allan <allan.averlan@laposte.net>
2022-06-22 15:53:48 +0200

    New Exo

---
+++ TP/Cours JS/Exercise/Exo_13/js/main.js
@@ -0,0 +1,9 @@
+/*
+Lecture de l'énoncé
+Analyse de la demande 
+Détermine la structure de la solution 
+Déterminer les traitements nécessaires
+Si il s'agit d'un afficahge , il faut le mettre en dur puis le dynamiser
+Si il y a des données iconnues dans l'affichage, il faut les chercher avant puis revenir les ajoutés dans l'affichage
+Faire les simplifications et/ou les factorisations nécessaires
+*/
