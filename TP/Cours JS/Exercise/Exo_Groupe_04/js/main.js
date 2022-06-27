78e90b9cb1f53630e15f2fc067648d8406ef6dfa
Allan <allan.averlan@laposte.net>
2022-06-27 13:59:59 +0200

    modif

--- TP/Cours JS/Exercise/Exo_Groupe_04/js/main.js
+++ TP/Cours JS/Exercise/Exo_Groupe_04/js/main.js
@@ -1,19 +1,18 @@
 //Construire la table de multiplication au format tableau html
 
-document.write("<table>"); // Création du TABLEAU
-
-for (let i = 1; i < 11; i++) { // Pour i =1; i inf à 11, incrémantation de  1
-    document.write("<tr>"); // Affichage d'un TR
-
-    for (let j = 1; j < 11; j++) { //Pour j =1; j inf à 11, incrémantation de  1
-        if (j == 1 || i == 1) {} // Si j OU i sont égal à 1
-        document.write(
-            "<td>" + i * j + "</td>"); // Affichage d'un TD avec le résultat de i * j
+function multiplication() {
+    document.write(`
+    <table>`);
+    for (let i = 1; i <= 10; i++) {
+        document.write(`<tr>`);
+        for (let j = 1; j <= 10; j++) {
+            document.write(`<td>${i * j}</td>`)
+        }
+        document.write(`</tr>`);
     }
-    document.write("</tr>"); // Fermeture du TR
+    document.write(`</table>`);
 }
-
-document.write("</table>"); // Fermeture du TABLEAU
+multiplication();
 
 
 /*
