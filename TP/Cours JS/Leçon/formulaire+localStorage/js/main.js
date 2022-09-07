397f173a439d49ed5a822c2a08f93f98ef1f687f
Allan <allan.averlan@laposte.net>
2022-09-07 12:05:46 +0200

    js

--- TP/Cours JS/Leçon/formulaire+localStorage/js/main.js
+++ TP/Cours JS/Leçon/formulaire+localStorage/js/main.js
@@ -7,6 +7,8 @@ document.querySelector("#users").innerHTML = localStorage.getItem("users");
 const myForm = document.querySelector("#my-form");
 myForm.addEventListener("submit", onSubmit);
 
+suppression();
+
 function onSubmit(e) {
   e.preventDefault();
 
@@ -37,26 +39,46 @@ function onSubmit(e) {
     // recuperer
     // localStorage.getItem("cle");
 
-    /*
-        1- recupérer les données du ls
-        2- ajouter un nouvel element aux données récupérées
-        3- enregistrer les données dans le ls (y compris le nouveau)
-        4- actualiser le visuel
-    */
+    // 1- recupérer les données du ls
+    // 2- ajouter un nouvel element aux données récupérées
+    // 3- enregistrer les données dans le ls (y compris le nouveau)
+    // 4- actualiser le visuel
 
     // 1 -
     let users =
       localStorage.getItem("users") !== null
         ? localStorage.getItem("users")
         : "";
+
     // 2-
     users += `<li><strong>${nameInput.value}</strong> :  ${emailInput.value}</li>`;
+
     // 3-  écraser l'ancien contenu du ls
     localStorage.setItem("users", users);
+
     //4 -
     ul.innerHTML = users;
 
+    suppression();
+
     nameInput.value = "";
     emailInput.value = "";
+  }
+}
+function suppression() {
+  //   //    if (!document.querySelector(".suppression")) == false  //  document.querySelector(".suppression") renvoie null
+  // dans un contexte booleen, null ets evaluée à false (utiliser le == et non pas ===)
+
+  if (!document.querySelector(".suppression")) {
+    if (localStorage.getItem("users") !== null) {
+      let button = document.createElement("span");
+      button.innerHTML = "<a class='btn suppression'>supprimer </a>";
+      myForm.appendChild(button);
+      button.addEventListener("click", () => {
+        localStorage.removeItem("users");
+        ul.innerHTML = "";
+        myForm.removeChild(button);
+      });
+    }
   }
 }
